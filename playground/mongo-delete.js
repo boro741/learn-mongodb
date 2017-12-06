3b891eb627473fdc8fe3a2b34ab86aa72f29d129
Pavan Boro <pavanboro14@gmail.com>
2017-12-06 20:26:05 +0530

    mongo delete

---
+++ playground/mongo-delete.js
@@ -0,0 +1,18 @@
+// const MongoClient = require('mongodb').MongoClient;
+// Using object destructuring:
+const {MongoClient , ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err,db)=>{
+    if(err){
+        return console.log('Unable to connect to MongoDB server');
+    }
+
+    console.log('Connected to MongoDB server');
+
+    db.collection('Users').findOneAndDelete({_id: new ObjectID('5a26c52c023ffe30b0689dc8')}).then( (result) => {
+        console.log(result);
+    });
+
+
+   // db.close();
+});
